9ddc1610d7023dc8cd17192b6ee9f438887e995d
jota <jotalabota@gmail.com>
2026-02-09 23:19:05 +0200

    Fix lspconfig removal

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,5 @@
 require("options")
 require("lz") -- Before keybindings to have which-key installed/loaded
 require("keybindings")
-require("lsp")
 require("custom.sessions")
 require("custom.zoom")
