f8917e0d0148042a81a17d3c447e4ad3fa41f045
jota <jotalabota@gmail.com>
2026-02-09 23:23:57 +0200

    Removed some lspconfig keybind

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -7,7 +7,6 @@ return {
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, { noremap = true })
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { noremap = true })
     vim.keymap.set('n', '<leader>la', vim.lsp.buf.code_action, { noremap = true })
-    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { noremap = true, silent = true })
 
     vim.diagnostic.config({
       virtual_text = false,
