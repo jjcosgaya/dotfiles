b5bf2721b8566b785ae4097cd4b2b4f220164ffc
jota <jotalabota@gmail.com>
2026-06-23 21:17:54 +0300

    Add float rule for notifications

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -301,6 +301,7 @@ floating_layout = layout.Floating(
     float_rules=[
         # Run the utility of `xprop` to see the wm class and name of an X client.
         *layout.Floating.default_float_rules,
+        Match(wm_type="notification"),
         Match(wm_class="confirmreset"),  # gitk
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
